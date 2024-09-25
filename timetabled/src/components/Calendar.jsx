f9ef8a7e855326b6168f59e6918bbeb215911740
Faith-Nchang <nchangfru24@gmail.com>
2024-09-24 19:26:56 -0700

    Created Calendar Component

---
+++ timetabled/src/components/Calendar.jsx
@@ -0,0 +1,141 @@
+import React from "react";
+import Event from './Event';
+
+
+const Calendar = () => {
+    return (
+        <div className="Calendar">
+          <h1>Time scheduling</h1>
+          <h2>Your number one vaction planning app</h2>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Sunday</th>
+                        <th>Monday</th>
+                        <th>Tuesday</th>
+                        <th>Wednesday</th>
+                        <th>Thursday</th>
+                        <th>Friday</th>
+                        <th>Saturday</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td className="time">8 am</td>
+                        <td>
+                        <Event event='Fancy Dinner ' color ='green'/>
+                        </td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td><Event event='Webinar on Marketing' color='pink' />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td className="time">9 am</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td><Event event='Code Review' color='blue' />
+                        </td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td className="time">10 am</td>
+                        <td></td>
+                        <td></td>
+                        <td><Event event='Morning Yoga' color='pink' />
+                        </td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td className="time">11 am</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td className="time">12 pm</td>
+                        <td>                         <td><Event event='Client Presentation' color='blue' /></td>
+                        </td>
+                        <td></td>
+                        <td></td>
+                        <td><Event event='Project Review' color='blue' />
+                        </td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td className="time">1 pm </td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td className="time">2 pm</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td>
+                        </td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td className="time">3 pm</td>
+                        <td></td>
+                        <td><Event event='Networking Event' color='blue' />
+                        </td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td><Event event='Dinner with Family' color='green' />
+                        </td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td className="time">4 pm</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td><Event event='Lunch with Friends' color='green' />
+                        </td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td className="time">5 pm</td>
+                        <td> <Event event='Budget Meeting' color='green' />
+                        </td>
+                        <td></td>
+                        <td> <Event event='Team Meeting' color='blue'/></td>
+                        <td> <td/>
+                        </td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    );
+}
+
+export default Calendar;
