02f8269cb8568a59577f059aa69b98c97a4f7fdd
Faith-Nchang <fnchang@montgomerycollege.edu>
2024-10-18 17:58:43 -0400

    added recipes from api call

--- RecipeGenius/src/App.jsx
+++ RecipeGenius/src/App.jsx
@@ -1,6 +1,9 @@
 import { useEffect, useState } from 'react'
 import Card from './Components/Card';
+import RecipeList from './Components/RecipeList';
+
 const API_KEY =   import.meta.env.VITE_APP_API_KEY
+
 
 import './App.css'
 
@@ -14,8 +17,20 @@ function App() {
     const getRecipes = async () => {
       const response = await fetch(url);
       const data = await response.json();
-      setRecipes(data.results);
-      console.log(data.results);
+      
+      const simplifiedRecipes = data.results.map(recipe => ({
+        title: recipe.title,
+        healthScore: recipe.healthScore,
+        servings: recipe.servings,
+        pricePerServing: recipe.pricePerServing,
+        image: recipe.image,
+        vegetarian: recipe.vegetarian,
+        timeToCook: recipe.readyInMinutes + " minutes"
+    }));
+    
+    // Now set the simplified recipes to the state
+    setRecipes(simplifiedRecipes);
+      console.log(simplifiedRecipes);
     }
     getRecipes().catch(console.error);
     
@@ -28,6 +43,7 @@ function App() {
         <h1>Recipe Genius</h1>
         
         <Card stastics="This is a test" />
+        <RecipeList recipes={recipes} />
       </div>
        
     </>
--- RecipeGenius/src/Components/RecipeList.jsx
+++ RecipeGenius/src/Components/RecipeList.jsx
@@ -1,9 +1,35 @@
 const RecipeList = ({ recipes }) => {
     return (
-        <div>
-            {recipes.map((recipe) => (
-                <RecipeCard key={recipe.id} recipe={recipe} />
-            ))}
+        <div className="list">
+            <table>
+            <thead>
+                <tr className="recipeHeader">
+                    <th>Recipe Name</th>
+                    <th>Health Score</th>
+                    <th>Servings</th>
+                    <th>Price Per Serving</th>
+                    <th>Vegetarian</th>
+                    <th>Time to Cook</th>
+                    <th>Image</th>
+                </tr>
+            </thead>
+            <tbody>
+                {recipes.map((recipe, index) => (
+                    <tr key={index} className="recipe">
+                        <td>{recipe.title}</td>
+                        <td>{recipe.healthScore}</td>
+                        <td>{recipe.servings}</td>
+                        <td>{recipe.pricePerServing}</td>
+                        <td>{recipe.vegetarian ? 'Yes' : 'No'}</td>
+                        <td>{recipe.timeToCook}</td>
+                        <td>
+                            <img src={recipe.image} alt={recipe.title} style={{ width: '100px' }} />
+                        </td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+
         </div>
     )
 }; // End of RecipeList
